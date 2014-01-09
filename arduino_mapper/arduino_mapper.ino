a86220207e50f555e8215b3bf3ff97b5f23247f7
denniss17 <dennisschroer@hotmail.com>
2014-01-09 11:47:54 +0100

    Knopjes

--- arduino_mapper/arduino_mapper.ino
+++ arduino_mapper/arduino_mapper.ino
@@ -1,18 +1,37 @@
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
+#include <EEPROM.h>
+
 int led = 13;
 int startButton = 6;
 int printButton = 7;
+byte running = 0;
+byte buttonState = 0;
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);  
-  pinMode(startButton, INPUT);
-  pinMode(printButton, INPUT);  
+void setup() {   
+    Serial.begin(9600);
+    // initialize the digital pin as an output.
+    pinMode(led, OUTPUT);  
+    pinMode(startButton, INPUT);
+    pinMode(printButton, INPUT);  
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(led, digitalRead(startButton));
+    if(digitalRead(startButton)==HIGH){
+        if(bitRead(buttonState,0)==0){
+            running = running==0 ? 1 : 0;
+            bitWrite(buttonState,0,1);
+        }
+    }else{
+        bitWrite(buttonState,0,0);
+    }
+    
+    if(digitalRead(printButton)==HIGH){
+        if(bitRead(buttonState,1)==0){
+            Serial.println("HAHA");
+            bitWrite(buttonState,1,1);
+        }
+    }else{
+        bitWrite(buttonState,1,0);
+    }
+
+    digitalWrite(led, running==0 ? LOW : HIGH);
 }
