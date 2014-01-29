ed601e921629bb87f71cf7b5193ba6fb675f0594
Nick ten Veen <nveen0930@gmail.com>
2014-01-29 17:10:34 +0100

    added robot logic

--- pathfinder.h
+++ pathfinder.h
@@ -2,9 +2,10 @@
 #include "map.h"
 
 #ifndef PATHFINDER_H
-#define PATHFINDER_DEBUG 0
+#define PATHFINDER_DEBUG 1
 #define PATHFINDER_H
 enum Heading { NORTH=0, WEST=1, SOUTH=2, EAST=3 };
+enum Direction {LEFT = 0, RIGHT = 1};
 
 struct targetCost{
     int cost;
@@ -25,6 +26,8 @@ class Pathfinder{
     int getTargetY();
     path getPath();
     int calculateTurningCost(int from, int to);
+	int calculateTurningDirection(int from, int to);
+	
     int getCostTo(int x, int y);
     int getTargetHeading(int x, int y);
     
